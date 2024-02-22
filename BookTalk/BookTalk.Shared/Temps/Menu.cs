624bf787ba4c79d1beb78b3480bc940073018b9f
jihun <jihun315016@gmail.com>
2024-02-22 17:26:01 +0900

    프로젝트 구성

---
+++ BookTalk/BookTalk.Shared/Temps/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTalk.Shared.Temps;
+
+public class Menu
+{
+    public int ID { get; set; }
+
+    public int Level { get; set; }
+
+    public string ControllerName { get; set; }
+
+    public string ActionName { get; set; }
+
+    public string MenuName { get; set; }
+
+    public int ParentMenuID { get; set; }
+}
---
+++ BookTalk/BookTalk.Shared/Temps/ResponseMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTalk.Shared.Temps;
+
+public class ResponseMessage
+{
+    public int ErrorCode { get; set; }
+
+    public string ErrorMessage { get; set; }
+}
+
+
+public class ResponseMessage<T>
+{
+    public int ErrorCode { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public T Data { get; set; }
+}
